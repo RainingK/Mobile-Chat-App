b9b235399e6943c937d4fa93ab3996a0d47bb50c
Ammar <ammar_98@live.com>
2023-05-14 20:37:17 +0400

    Using Tabs

--- app/src/main/java/com/example/mobilechatapp/MainActivity.kt
+++ app/src/main/java/com/example/mobilechatapp/MainActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = "Running App"
-    }
+		val textView: TextView = findViewById(R.id.textView)
+		textView.text = "Running App"
+	}
 }
