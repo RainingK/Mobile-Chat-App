4bfd173f5877fd9027098ea613d2bba70ea45d79
Ammar <ammar_98@live.com>
2023-05-20 15:06:57 +0400

    Shows List Of Users

--- app/src/main/java/com/example/mobilechatapp/MainActivity.kt
+++ app/src/main/java/com/example/mobilechatapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.mobilechatapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobilechatapp.adapter.UserAdapter
 import com.example.mobilechatapp.classes.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var userRecyclerView: RecyclerView
@@ -17,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 	private lateinit var userAdapter: UserAdapter
 
 	private lateinit var auth: FirebaseAuth
+	private lateinit var db: DatabaseReference
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -27,6 +33,31 @@ class MainActivity : AppCompatActivity() {
 		userAdapter = UserAdapter(this, userList)
 
 		auth = FirebaseAuth.getInstance()
+		db = FirebaseDatabase.getInstance().reference
+
+		userRecyclerView.layoutManager = LinearLayoutManager(this)
+		userRecyclerView.adapter = userAdapter
+
+		db.child("users").addValueEventListener(object : ValueEventListener {
+			override fun onDataChange(snapshot: DataSnapshot) {
+				userList.clear()
+
+				for (ds in snapshot.children) {
+					val currentUser = ds.getValue(User::class.java)
+
+					if (currentUser?.getUid() != auth.currentUser?.uid) {
+						userList.add(currentUser!!)
+					}
+				}
+
+				userAdapter.notifyDataSetChanged()
+			}
+
+			override fun onCancelled(error: DatabaseError) {
+
+			}
+
+		})
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
